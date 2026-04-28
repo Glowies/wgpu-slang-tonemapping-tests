0114d106510e4778020623468d2cb9e253bea0d8
Oktay Comu <self@oktaycomu.com>
2026-04-28 07:29:56 -0400

    feat: pass input & output params as uniforms
    Signed-off-by: Oktay Comu <self@oktaycomu.com>

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
 use clap::Parser;
 use pollster::FutureExt;
-use wgpu_slang_aces2::Args;
+use wgpu_slang_tonemappers::Args;
 
 fn main() {
     let args = Args::parse();
-    wgpu_slang_aces2::run(args).block_on().unwrap();
+    wgpu_slang_tonemappers::run(args).block_on().unwrap();
 }
